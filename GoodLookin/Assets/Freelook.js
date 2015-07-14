bb0c0dfde040ad7b8f2bde1d7d95f20ff0a126e5
watergun127 <samuel@winslowstark.org>
2015-07-14 10:28:28 +0100

    Added 'GoodLookin' to repo, with custom gitignore

---
+++ GoodLookin/Assets/Freelook.js
@@ -0,0 +1,51 @@
+﻿var flySpeed:float = 0.5;
+var shift : boolean;
+var ctrl : boolean;
+var accelerationAmount : float = 3;
+var accelerationRatio : float = 1;
+var slowDownRatio : float = 0.5;
+function Start(){
+	Screen.lockCursor=true;
+}
+function Update()
+{
+	transform.Rotate(Vector3.up*Input.GetAxis("Mouse X"),Space.World);
+	transform.Rotate(Vector3.right*Input.GetAxis("Mouse Y"),Space.World);
+    if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+    {
+        shift = true;
+        flySpeed *= accelerationRatio;
+    }
+   
+    if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+    {
+        shift = false;
+        flySpeed /= accelerationRatio;
+    }
+    if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
+    {
+        ctrl = true;
+        flySpeed *= slowDownRatio;
+    }
+    if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl))
+    {
+        ctrl = false;
+        flySpeed /= slowDownRatio;
+    }
+    if (Input.GetAxis("Vertical") != 0)
+    {
+        transform.Translate(-transform.forward * flySpeed * Input.GetAxis("Vertical"));
+    }
+    if (Input.GetAxis("Horizontal") != 0)
+    {
+        transform.Translate(-transform.right * flySpeed * Input.GetAxis("Horizontal"));
+    }
+    if (Input.GetKey(KeyCode.E))
+    {
+        transform.Translate(transform.up * flySpeed*0.5f);
+    }
+    else if (Input.GetKey(KeyCode.Q))
+    {
+        transform.Translate(-transform.up * flySpeed*0.5f);
+    }
+}
---
+++ GoodLookin/Assets/Graphics Resources/Shaders/Double Sided.shader
@@ -0,0 +1,46 @@
+﻿Shader "Unlit/Double Sided" {
+    Properties {
+        _MainTex ("Base (RGB)", 2D) = "white" {}
+    }
+    SubShader {    
+        //UsePass "Unlit/Texture"
+       
+        // Ambient pass
+        Pass {
+        Name "BASE"
+        Tags {"LightMode" = "Always" /* Upgrade NOTE: changed from PixelOrNone to Always */}
+        Color [_PPLAmbient]
+        SetTexture [_BumpMap] {
+            constantColor (.5,.5,.5)
+            combine constant lerp (texture) previous
+            }
+        SetTexture [_MainTex] {
+            constantColor [_Color]
+            Combine texture * previous DOUBLE, texture*constant
+            }
+        }
+   
+    // Vertex lights
+    Pass {
+        Name "BASE"
+        Tags {"LightMode" = "Vertex"}
+        Material {
+            Diffuse [_Color]
+            Emission [_PPLAmbient]
+            Shininess [_Shininess]
+            Specular [_SpecColor]
+            }
+        SeparateSpecular On
+        Lighting On
+        Cull Off
+        SetTexture [_BumpMap] {
+            constantColor (.5,.5,.5)
+            combine constant lerp (texture) previous
+            }
+        SetTexture [_MainTex] {
+            Combine texture * previous DOUBLE, texture*primary
+            }
+        }
+    }
+    FallBack "Diffuse", 1
+}
